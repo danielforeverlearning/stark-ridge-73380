ccfbc85b9f060dc1254ae91354bc8b5a5e5c6b99
Max Lee <maxlee@pacrim.edu>
2018-02-28 11:31:15 -0800

    new parse overload

--- index.js
+++ index.js
@@ -8,23 +8,45 @@ const PORT     = process.env.PORT || 5000
 http.createServer(function (req, res) {
   if (req.url == '/fileupload') {
     var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.path;
-      res.write('oldpath is');
-      res.write(oldpath);
-      res.end();
+
+    //form.parse(req, function (err, fields, files) {
+    //  var oldpath = files.filetoupload.path;
+    //  res.write('oldpath at heroku is ');
+    //  res.write(oldpath);
+    //  res.end();
+
+        //***** you do not know accessible filesystem at heroku machines *****
+        //var newpath = '/home/max/nodejs_code/stark-ridge-73380/' + files.filetoupload.name;
+        //fs.rename(oldpath, newpath, function (err) {
+        //  if (err) throw err;
+        //  res.write('File uploaded and moved!');
+        //  res.end();
+        //});
       
-      //var newpath = '/home/max/nodejs_code/stark-ridge-73380/' + files.filetoupload.name;
-      //fs.rename(oldpath, newpath, function (err) {
-      //  if (err) throw err;
-      //  res.write('File uploaded and moved!');
-      //  res.end();
-      //});
-    });
+    //});
+
+    form.parse(req)
+        .on('field', function(name,field) {
+            console.log('Got a field:', name);
+        })
+        .on('file', function(name,file) {
+            console.log('Got file:', name);
+        })
+        .on('error', function(err) {
+            console.log('Got error: ');
+            console.log(err);
+            res.send({'success': false, error: err});
+        })
+        .on('end', function() {
+            console.log('Got end');
+            res.send({'success': true});
+        });
+
   } else {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
     res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="text" name="aaa_name" id="aaa_id"><br>');
     res.write('<input type="submit">');
     res.write('</form>');
     return res.end();
