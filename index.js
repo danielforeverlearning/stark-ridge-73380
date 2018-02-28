1822c6a7e7918a59a0a32666d7f95b0c677e4b8e
Max Lee <maxlee@pacrim.edu>
2018-02-28 12:10:05 -0800

    debugging

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
     res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<label for="aaa_id">aaa</label>');
     res.write('<input type="text" name="aaa_name" id="aaa_id"><br>');
     res.write('<input type="submit">');
     res.write('</form>');
